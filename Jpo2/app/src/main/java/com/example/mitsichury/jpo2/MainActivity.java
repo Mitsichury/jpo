687f677ab8757223441d2fdc6dd39598a16a704b
Mitsichury <mitsichury@gmail.com>
2015-09-24 15:36:32 +0200

    Fichier snake

--- Jpo2/app/src/main/java/com/example/mitsichury/jpo2/MainActivity.java
+++ Jpo2/app/src/main/java/com/example/mitsichury/jpo2/MainActivity.java
@@ -45,24 +45,7 @@ public class MainActivity extends Activity implements SensorEventListener{
 
         minuteur = (TextView)findViewById(R.id.textView);
         zoneDeDessin = (ZoneDeDessin)findViewById(R.id.drawview);
-        // Faut quand meme pouvoir sortir du jeux
-        zoneDeDessin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        getWindow().getDecorView().setSystemUiVisibility(
-                                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
-                    }
-                }, 1000);
-            }
-        });
+
         boutonRecommencer = (Button)findViewById(R.id.boutonRecommncer);
 
         boutonRecommencer.setOnClickListener(new View.OnClickListener() {
@@ -95,22 +78,7 @@ public class MainActivity extends Activity implements SensorEventListener{
 
         deuxChiffresPourLesSecondes = new DecimalFormat("00");
 
-        agrandirEcran();
-
         nouveauJeu();
-    }
-
-    /**
-     * Cache la barre de navigation + bouton principaux
-      */
-    private void agrandirEcran() {
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
     }
 
     /**
