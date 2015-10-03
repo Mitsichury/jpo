366b871e4959f0ed098b8847d567b07d9fa6141f
Mitsichury <mitsichury@gmail.com>
2015-10-03 16:27:59 +0200

    jpo

--- Jpo2/app/src/main/java/com/example/mitsichury/jpo2/MainActivity.java
+++ Jpo2/app/src/main/java/com/example/mitsichury/jpo2/MainActivity.java
@@ -65,7 +65,6 @@ public class MainActivity extends Activity{
             }
         });
 
-
         boutonRecommencer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
